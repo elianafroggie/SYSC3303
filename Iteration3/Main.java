0b224a9a970e76adc64d91a3edb90d6659af357a
elianafroggie <elianaschartner@cmail.carleton.ca>
2024-03-12 12:18:01 -0400

    Update Main.java

--- Iteration3/Main.java
+++ Iteration3/Main.java
@@ -1,16 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         // Instantiate objects of the client, host, and server classes
-        ClientFloor client = new ClientFloor();
-        HostScheduler host = new HostScheduler();
-        ServerElevator[] servers = new ServerElevator[3];
-        for (int i = 0; i < servers.length; i++) {
-            servers[i] = new ServerElevator(i);
-            servers[i].startThreads();
-        }
+        Floor client = new Floor();
+        Scheduler host = new Scheduler();
 
         // Start the processes or threads (the functions for each class)
-        Thread clientFloorThread = new Thread(() -> client.readCSV("", "", 1026));
+        Thread clientFloorThread = new Thread(() -> client.readCSV("data.csv", ",", 1026));
         Thread hostSchedulerThread = new Thread(host::schedulerRequests);
 
         // Start the threads
