c7cf3b9ccc2a66b6596d114f72d1cecf38fd70ec
elianafroggie <elianaschartner@cmail.carleton.ca>
2024-04-10 18:23:46 -0400

    Create ElevatorMain.java

---
+++ Iteration5/Seperate_Computers/ElevatorMain.java
@@ -0,0 +1,40 @@
+//package org.example;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ElevatorSubsystem subsystem = new ElevatorSubsystem(4);
+
+        // Instantiates the GUIForElevator object with 4 elevators, which sets up the GUI for displaying the real-time info about each elevator present in the control system
+        GUIForElevator elevatorGUI = new GUIForElevator(4);
+
+        // Start the processes or threads (the functions for each class)
+        Thread subsystemThread = new Thread(subsystem::sendReceiveRequests);
+
+        // Start the threads
+        subsystemThread.start();
+
+
+        // Simulation of the movement of the elevators over time, and performing an iteration through the time in order to update the elevator positions accurately
+        while(true) {
+            String elevatorDataString = subsystem.getInfo();
+            String[] elevatorSubsystemParts = elevatorDataString.split(",");
+
+            for(int i = 0; i < elevatorSubsystemParts.length - 1; i += 6){
+                int elevatorId = Integer.parseInt(elevatorSubsystemParts[i].trim());
+                String state = elevatorSubsystemParts[1+i].trim();
+                int currentFloor = Integer.parseInt(elevatorSubsystemParts[3+i].trim());
+                int passengers = Integer.parseInt(elevatorSubsystemParts[5+i].trim());
+                elevatorGUI.updateElevatorLocation(elevatorId, currentFloor, state, passengers);
+            }
+
+            try {Thread.sleep(1000);
+
+            } catch (InterruptedException interruptedException) {
+                // Print any interrupted exceptions that can occur during the thread sleep time of 1500
+                interruptedException.printStackTrace();
+            }
+        }
+    }
+}
